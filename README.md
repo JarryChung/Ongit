539d7a913e4a795ae2dfea47350b4e77e02af499
JarryChung <jarrychung@outlook.com>
2022-03-12 23:17:49 +0800

    feat: complete the core features

---
+++ README.md
@@ -0,0 +1,20 @@
+# ongit
+
+Wake up the browser in the terminal and open the git repository of the current/specified folder
+
+## Install
+
+```shell
+npm install -g @tsnp/ongit
+```
+
+## Usage
+
+1. For the current folder `ongit`
+2. For the specified folder `ongit path/to/your/folder`
+
+## Error Code
+
+**-1**: Maybe this folder is not a git project.
+**-2**: The git project does not have a origin address set.
+**-3**: The origin address may be illegal or the program has a bug, please create issue [here](https://github.com/JarryChung/Ongit/issues).
---
+++ ongit.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const childProcess = require('child_process');
+const os = require('os');
+
+const getPath = () => {
+  return process.argv[2] || '.';
+};
+
+const getGitURL = (gitPath) => {
+  let url = '';
+  try {
+    url = childProcess.execSync(`git -C ${gitPath} remote -v`).toString();
+    if (!url) {
+      console.log('[ongit] This git project does not have a origin address set');
+      process.exit(-2);
+    }
+  } catch (error) {
+    console.log('[ongit] This folder does not appear to be a git project');
+    process.exit(-1);
+  }
+  return url;
+};
+
+const getTarget = (gitUrl) => {
+  const line1 = gitUrl.split('\n')[0];
+  if (!line1) {
+    console.log('[ongit] An error occurred while getting the origin address');
+    process.exit(-3);
+  }
+  let target = line1.split('.git')[0];
+  if (target.indexOf('git@')) {
+    target = `https://${target.split('git@')[1].replace(':', '/')}`;
+  }
+  return target;
+};
+
+const getCommand = () => {
+  const COMMAND_MAP = {
+    darwin: 'open',
+    win32: 'start',
+    default: 'xdg-open',
+  };
+  const platform = os.platform();
+  return COMMAND_MAP[platform] || platform.default;
+};
+
+const gitPath = getPath();
+const url = getGitURL(gitPath);
+const target = getTarget(url);
+const command = getCommand();
+childProcess.execSync(`${command} ${target}`);
